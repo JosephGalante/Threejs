2ddc70fb3b1ab81f80795656db241b3532f34d69
Joe Galante <JEG150@pitt.edu>
2022-03-22 11:50:39 -0400

    Initial Commit

--- Transform Objects/05-transform-objects/src/script.js
+++ Transform Objects/05-transform-objects/src/script.js
@@ -10,10 +10,57 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 0xff0000})
+)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 0xff00ff})
+)
+cube2.position.x = -2
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 0x0000ff})
+)
+cube3.position.x = 2
+group.add(cube3)
+
+// Position Objects
+// mesh.position.z = -2
+// mesh.position.x = -2
+// mesh.position.y = -2
+// mesh.position.set(1, -0.6, -1)
+// scene.add(mesh)
+
+// Axes Helper
+const axesHelper = new THREE.AxesHelper(1)
+scene.add(axesHelper)
+
+// Scale Objects
+// mesh.scale.x = 2
+// mesh.scale.y = 0.5
+// mesh.scale.z = 0.5
+// mesh.scale.set(2, 0.5, 0.5)
+
+// Rotate objects
+// mesh.rotation.x = Math.PI * 0.25
+// mesh.rotation.y = Math.PI * 0.25
+// mesh.rotation.z = Math.PI * 0.25
+
 
 /**
  * Sizes
@@ -28,7 +75,12 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+camera.position.y = 0
+camera.position.x = 0
 scene.add(camera)
+
+// camera.lookAt(mesh.position)
+
 
 /**
  * Renderer
